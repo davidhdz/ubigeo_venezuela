9bc1c51bd043458bc74acd705d2dbc6a23f0daaf
David Hernandez <david.vzla@gmail.com>
2024-11-14 13:29:30 -0400

    [FIX] Arreglar fallos de linter

--- app/main.py
+++ app/main.py
@@ -1,13 +1,29 @@
+# docstring this module
+"""Module to create the FastAPI application"""
+
 from fastapi import FastAPI
-from app.routers import ubigeo_v1 
+from app.routers import ubigeo_v1
 
 
 def create_app():
-    app = FastAPI()
+    """
+    Create the FastAPI application, attach the UBIGEO VENEZUELA v1 endpoints and return it.
 
-    app.include_router(ubigeo_v1.router, prefix="/v1")
-
-    return app
+    Returns:
+        FastAPI: The FastAPI application.
+    """
+    ubi_api = FastAPI(
+        title="UbiGeo Venezuela API",
+        description="## API for ubigeo venezuelan entities",
+        version="1.0.0",
+        contact={
+            "name": "by David Hernández Aponte",
+            "url": "https://github.com/davidhdz/ubigeo_venezuela",
+        },
+        redoc_url=None,
+    )
+    ubi_api.include_router(ubigeo_v1.router, prefix="/v1")
+    return ubi_api
 
 
 app = create_app()
--- app/routers/ubigeo_v1.py
+++ app/routers/ubigeo_v1.py
@@ -1,18 +1,43 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""
+This module provides a FastAPI router for retrieving data from a JSON file 
+containing Venezuelan's administrative divisions (UBIGEO).
+
+It includes the following endpoints:
+
+- `/all`: Returns all the data in the JSON file.
+- `/states`: Returns a list of all the state names.
+- `/by_id/state/{id}`: Returns the state data by its ID.
+- `/by_name/{state}`: Returns the state data by its name.
+- `/by_id/municipality/{id}`: Returns the municipality data by its ID.
+- `/by_id/parish/{id}`: Returns the parish data by its ID.
+
+The JSON file is located at `/code/app/data/ubigeo_ven.json`.
+
+This module requires the following dependencies:
+- fastapi
+- unidecode
+
+Example usage:
 
 from fastapi import FastAPI
+from app.routers.ubigeo_v1 import ubigeo_v1_router
+
+app = FastAPI()
+app.include_router(ubigeo_v1_router, prefix="/v1")
+"""
+
 import json
-from unidecode import unidecode
+
 from fastapi import APIRouter
+from unidecode import unidecode
 
 router = APIRouter(
     responses={404: {"description": "Not found"}},
 )
 
 DATA_FILE = "/code/app/data/ubigeo_ven.json"
-
-import json
 
 
 def read_json_file(file_path):
@@ -26,15 +51,16 @@ def read_json_file(file_path):
         **dict**: The loaded JSON data, or None if the file is not found.
     """
     try:
-        with open(file_path, "r") as file:
+        with open(file_path, "r", encoding="utf-8") as file:
             return json.load(file)
     except FileNotFoundError as e:
+        print(f"Error: {e}")
         print(f"File not found: {file_path}")
         return None
 
 
 @router.get("/all", tags=["all data"])
-async def getAll():
+async def get_all():
     """
     A function to retrieve all data.
     """
@@ -58,115 +84,116 @@ async def get_list_states():
     return states
 
 
-@router.get("/by_id/state/{id}", tags=["states"])
-async def get_item_state_by_id(id: str):
+@router.get("/by_id/state/{state_id}", tags=["states"])
+async def get_item_state_by_id(state_id: str):
     """
     An asynchronous function to retrieve the state by ID from the specified JSON file.
 
     **Args**:
-    - **id (str)**: The ID of the state to retrieve.
+    - **state_id (str)**: The ID of the state to retrieve.
 
     **Returns**:
     - **dict**: The state information if found, otherwise False.
     """
     if json_data := read_json_file(DATA_FILE):
         for state in json_data["estados"]:
-            if state["codigo"] == id:
+            if state["codigo"] == state_id:
                 return state
     return False
 
 
-@router.get("/by_name/{state}", tags=["states"])
-async def get_item_state_by_name(state: str):
+@router.get("/by_name/{state_name}", tags=["states"])
+async def get_item_state_by_name(state_name: str):
     """
     An asynchronous function to retrieve the state item by name from the JSON data.
 
     **Parameters**:
-    - **state (str)**: The name of the state to retrieve.
+    - **state_name (str)**: The name of the state to retrieve.
 
     **Returns**:
     - **dict**: The state item corresponding to the given state name, or False if not found.
     """
     if json_data := read_json_file(DATA_FILE):
         for state_item in json_data["estados"]:
-            if unidecode(state_item["nombre"].lower()) == unidecode(state.lower()):
+            if unidecode(state_item["nombre"].lower()) == unidecode(state_name.lower()):
                 return state_item
     return False
 
 
-@router.get("/by_id/municipality/{id}", tags=["municipalities"])
-async def get_item_municipality_by_id(id: str):
+@router.get("/by_id/municipality/{municipality_id}", tags=["municipalities"])
+async def get_item_municipality_by_id(municipality_id: str):
     """
     Asynchronous function to retrieve a municipality by its ID.
 
     **Args**:
-    - **id (str)**: The ID of the municipality.
+    - **municipality_id (str)**: The ID of the municipality.
 
     **Returns**:
     - **dict**: The municipality data including its state code.
     - **bool**: False if the municipality data is not found.
     """
-    state_code = id[0:2]
-    if state := await get_item_state_by_id(id=state_code):
+    state_code = municipality_id[0:2]
+    if state := await get_item_state_by_id(state_id=state_code):
         data = {}
         for municipality in state["municipios"]:
-            if municipality["codigo"] == id:
+            if municipality["codigo"] == municipality_id:
                 data = municipality
                 data["codigo_estado"] = state_code
                 return data
     return False
 
 
-@router.get("/by_name/{state}/{municipality}", tags=["municipalities"])
-async def get_item_municipality_by_name(state: str, municipality: str):
+@router.get("/by_name/{state_name}/{municipality_name}", tags=["municipalities"])
+async def get_item_municipality_by_name(state_name: str, municipality_name: str):
     """
     Asynchronous function to retrieve municipality data by name and state.
 
     **Args**:
-    - **state (str)**: The state name.
-    - **municipality (str)**: The municipality name.
+    - **state_name (str)**: The state name.
+    - **municipality_name (str)**: The municipality name.
 
     **Returns**:
     - **dict**: The municipality data including its state code.
     - **bool**: False if the municipality data is not found.
     """
-    if state_data := await get_item_state_by_name(state=state):
+    if state_data := await get_item_state_by_name(state_name=state_name):
         data = {}
         for municipality_item in state_data["municipios"]:
             if unidecode(municipality_item["nombre"].lower()) == unidecode(
-                municipality.lower()
+                municipality_name.lower()
             ):
                 data = municipality_item
                 data["codigo_estado"] = municipality_item["codigo"][0:2]
                 return municipality_item
-            else:
-                for nombres_alternos in municipality_item["nombres_alternos"]:
-                    if unidecode(nombres_alternos.lower()) == unidecode(
-                        municipality.lower()
-                    ):
-                        data = municipality_item
-                        data["codigo_estado"] = municipality_item["codigo"][0:2]
-                        return municipality_item
+            for nombres_alternos in municipality_item["nombres_alternos"]:
+                if unidecode(nombres_alternos.lower()) == unidecode(
+                    municipality_name.lower()
+                ):
+                    data = municipality_item
+                    data["codigo_estado"] = municipality_item["codigo"][0:2]
+                    return municipality_item
     return False
 
 
-@router.get("/by_id/parish/{id}", tags=["parish"])
-async def get_item_parish_by_id(id: str):
+@router.get("/by_id/parish/{parish_id}", tags=["parish"])
+async def get_item_parish_by_id(parish_id: str):
     """
     Asynchronous function to retrieve parish data by ID from the specified municipality.
 
     **Args**:
-    - **id (str)**: The ID of the parish to retrieve.
+    - **parish_id (str)**: The ID of the parish to retrieve.
 
     **Returns**:
     - **dict**: The data of the parish including its municipality and state codes.
     - **bool**: False if the parish data is not found.
     """
-    municipality_code = id[0:4] + "00"
-    if municipality := await get_item_municipality_by_id(id=municipality_code):
+    municipality_code = parish_id[0:4] + "00"
+    if municipality := await get_item_municipality_by_id(
+        municipality_id=municipality_code
+    ):
         data = {}
         for parish in municipality["parroquias"]:
-            if parish["codigo"] == id:
+            if parish["codigo"] == parish_id:
                 data = parish
                 data["codigo_municipio"] = municipality_code
                 data["codigo_estado"] = municipality["codigo_estado"]
@@ -174,35 +201,39 @@ async def get_item_parish_by_id(id: str):
     return False
 
 
-@router.get("/by_name/{state}/{municipality}/{parish}", tags=["parish"])
-async def get_item_parish(state: str, municipality: str, parish: str):
+@router.get("/by_name/{state_name}/{municipality_name}/{parish_name}", tags=["parish"])
+async def get_item_parish(state_name: str, municipality_name: str, parish_name: str):
     """
-    Asynchronous function to retrieve parish data based on the state, municipality, and parish names.
+    Asynchronous function to retrieve parish data based on the state,
+    municipality, and parish names.
 
     **Args**:
-    - **state (str)**: The name of the state.
-    - **municipality (str)**: The name of the municipality.
-    - **parish (str)**: The name of the parish.
+    - **state_name (str)**: The name of the state.
+    - **municipality_name (str)**: The name of the municipality.
+    - **parish_name (str)**: The name of the parish.
 
     **Returns**:
     - **dict**: The data of the specified parish including its municipality and state codes.
     - **bool**: False if the parish data is not found.
     """
     if municipality_data := await get_item_municipality_by_name(
-        state=state, municipality=municipality
+        state_name=state_name, municipality_name=municipality_name
     ):
         data = {}
         for parish_item in municipality_data["parroquias"]:
-            if unidecode(parish_item["nombre"].lower()) == unidecode(parish.lower()):
+            if unidecode(parish_item["nombre"].lower()) == unidecode(
+                parish_name.lower()
+            ):
                 data = parish_item
                 data["codigo_municipio"] = municipality_data["codigo"]
                 data["codigo_estado"] = municipality_data["codigo_estado"]
                 return data
-            else:
-                for nombres_alternos in parish_item["nombres_alternos"]:
-                    if unidecode(nombres_alternos.lower()) == unidecode(parish.lower()):
-                        data = parish_item
-                        data["codigo_municipio"] = municipality_data["codigo"]
-                        data["codigo_estado"] = municipality_data["codigo_estado"]
-                        return data
+            for nombres_alternos in parish_item["nombres_alternos"]:
+                if unidecode(nombres_alternos.lower()) == unidecode(
+                    parish_name.lower()
+                ):
+                    data = parish_item
+                    data["codigo_municipio"] = municipality_data["codigo"]
+                    data["codigo_estado"] = municipality_data["codigo_estado"]
+                    return data
     return False
